c86b2f76aa2d4ef4085baaf154f5109d8dddceea
Mauko Maunde <maukoese@gmail.com>
2024-10-13 03:48:33 +0300

    Update dependency versions to support Laravel 11

--- tests/seeds/factory.php
+++ tests/seeds/factory.php
@@ -1,36 +1,31 @@
 <?php
 
-use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factory;
-
-$factory = app(Factory::class);
-
-$factory->define(Tests\Models\User::class, function (Faker $faker) {
+$factory->define(Tests\Models\User::class, function () {
     return [
-        'username' => $faker->userName,
-        'email'    => $faker->email,
-        'mobile'   => $faker->phoneNumber,
-        'avatar'   => $faker->imageUrl(),
+        'username' => fake()->userName(),
+        'email'    => fake()->email(),
+        'mobile'   => fake()->phoneNumber(),
+        'avatar'   => fake()->imageUrl(),
         'password' => '$2y$10$U2WSLymU6eKJclK06glaF.Gj3Sw/ieDE3n7mJYjKEgDh4nzUiSESO', // bcrypt(123456)
     ];
 });
 
-$factory->define(Tests\Models\Profile::class, function (Faker $faker) {
+$factory->define(Tests\Models\Profile::class, function () {
     return [
-        'first_name' => $faker->firstName,
-        'last_name'  => $faker->lastName,
-        'postcode'   => $faker->postcode,
-        'address'    => $faker->address,
-        'latitude'   => $faker->latitude,
-        'longitude'  => $faker->longitude,
-        'color'      => $faker->hexColor,
-        'start_at'   => $faker->dateTime,
-        'end_at'     => $faker->dateTime,
+        'first_name' => fake()->firstName(),
+        'last_name'  => fake()->lastName(),
+        'postcode'   => fake()->postcode(),
+        'address'    => fake()->address(),
+        'latitude'   => fake()->latitude(),
+        'longitude'  => fake()->longitude(),
+        'color'      => fake()->hexColor(),
+        'start_at'   => fake()->dateTime(),
+        'end_at'     => fake()->dateTime(),
     ];
 });
 
-$factory->define(Tests\Models\Tag::class, function (Faker $faker) {
+$factory->define(Tests\Models\Tag::class, function () {
     return [
-        'name' => $faker->word,
+        'name' => fake()->word,
     ];
 });
